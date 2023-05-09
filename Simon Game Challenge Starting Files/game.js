79ca603b8c586362dc5e04341406e7b4d55e673b
aditipatni <aditipatni311@gmail.com>
2023-05-09 12:26:15 +0530

    demo

---
+++ Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,12 @@
+var buttonColors=["red", "blue", "green", "yellow"];
+var gamePattern = [];
+
+var randomNumber;
+function nextSequence(){
+    return randomNumber=Math.floor(Math.random()*4);
+    var randomChosenColor=buttonColors[randomNumber];
+
+    gamePattern.push(randomChosenColour);
+}
+    
+
