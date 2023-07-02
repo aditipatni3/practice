43b1fe51573e8e951b47b45214cf63c6b7813386
Aditi Patni <91011940+aditipatni3@users.noreply.github.com>
2023-07-02 20:24:43 +0530

    Add files via upload

---
+++ calci/calculator.js
@@ -0,0 +1,19 @@
+const express= require("express");
+const bodyParser=require("body-parser");
+
+const app=express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.listen(3000)
+app.get("/", function(req, res){
+    res.sendFile(__dirname+"/index.html");
+})
+
+app.post("/", function(req, res){
+    var num1= Number(req.body.n1)
+    var num2= Number(req.body.n2)
+    var result=num1+num2;
+    res.send("result is "+ result);
+
+})
